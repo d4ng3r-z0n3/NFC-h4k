03d42124ea8e9b3c72a8c8e87fbb975cafc719bf
Glitch (hello-webpage) <none>
2019-08-13 12:25:14 +0000

    🍘🍍 Checkpoint ./script.js:244840/205

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const r = new NFCReader({compatibility: 'any'});
 r.addEventListener('reading', event => {
   console.log(event);
+  document.body.textContent = 'reading';
+
 });
 r.start();
+document.body.textContent = new Date();
