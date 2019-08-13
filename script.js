869e7c86d65eed4abd863abc865de8912583dd2c
Glitch (hello-webpage) <none>
2019-08-13 13:53:13 +0000

    💋👦 Checkpoint ./index.html:244840/100 ./script.js:244840/52

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const r = new NFCReader({compatibility: 'any'});
+const r = new NFCReader({ compatibility: 'any' });
 r.addEventListener('reading', event => {
   console.log(event);
-  document.body.textContent += 'reading\n';
+  pre.textContent += 'reading\n';
 });
 r.addEventListener('error', event => {
   console.log(event);
-  document.body.textContent += event.error + '\n';
+  pre.textContent += event.error + '\n';
 });
 r.start();
-document.body.textContent = 'WebNFC - crbug.com/993327\n';
+pre.textContent = 'WebNFC - crbug.com/993327\n';
