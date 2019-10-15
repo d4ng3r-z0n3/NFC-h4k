082d48a0b6ecc022e77844b43c31843947601a05
Glitch (hello-webpage) <none>
2019-10-15 14:08:38 +0000

    🍧💏 Checkpoint ./script.js:244840/14

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ writeButton.addEventListener("click", async _ => {
       records: [
         {
           recordType: "url",
-          data: "https://mobile.twitter.com"
+          data: "https://mobile.twitter.com/manifest.json"
         }
       ]
     });
