f21c06aa8c6865b5796f58dc4ac6c81440267cbd
Glitch (hello-webpage) <none>
2019-10-22 04:47:34 +0000

    🛋👷 Checkpoint ./external-records.js:244840/4221

--- external-records.js
+++ external-records.js
@@ -6,9 +6,9 @@ reader.scan({
 reader.onreading = event => {
   const decoder = new TextDecoder();
   for (const record of event.message.records) {
-    if (record.mediaType === 'application/json') {
+    if (record.mediaType === "application/json") {
       const json = JSON.parse(decoder.decode(record.data));
-      const article =/^[aeio]/i.test(json.title) ? "an" : "a";
+      const article = /^[aeio]/i.test(json.title) ? "an" : "a";
       console.log(`${json.name} is ${article} ${json.title}`);
     }
   }
@@ -21,41 +21,45 @@ writer.push({
     {
       recordType: "media",
       mediaType: "application/json",
-      data: encoder.encode(JSON.stringify({
-        name: "Benny Jensen",
-        title: "Banker"
-      }))
+      data: encoder.encode(
+        JSON.stringify({
+          name: "Benny Jensen",
+          title: "Banker"
+        })
+      )
     },
     {
       recordType: "media",
       mediaType: "application/json",
-      data: encoder.encode(JSON.stringify({
-        name: "Zoey Braun",
-        title: "Engineer"
-      }))
-    }]
+      data: encoder.encode(
+        JSON.stringify({
+          name: "Zoey Braun",
+          title: "Engineer"
+        })
+      )
+    }
+  ]
 });
 
-
-writer.push({ records: [
-  {
-    recordType: "external",
-    data: {
-      records: [
-        {
-          recordType: "example.com:shoppingItem",
-          data: Uint8rray.of(...1234)
-        },
-        {
-          recordType: "example.com:shoppingItem",
-          data: "Game context given here"
-        },
-        {
-          recordType: "media",
-          mediaType: "image/png"
-          data: getImageBytes(fromURL);
-        }
-      ]
+// actually I guess that unknown records could be useful inside external records
+// as its your record, so you know what it represents and can avoid storing the mime type\
+const writer = NDEFWriter();
+writer.push({
+  records: [
+    {
+      recordType: "example.com:shoppingItem", // External record
+      data: {
+        records: [
+          {
+            recordType: "unknown", // Shopping ID
+            data: Uint8Array.of(...1234)
+          },
+          {
+            recordType: "unknown", // Shopping item description
+            data: new TextEncoder().encode("NFC Tag Sticker")
+          }
+        ]
+      }
     }
-  }
-]});
+  ]
+});
